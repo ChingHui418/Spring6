9a67393b963e41b01e8cd62d38826a2e965ad7bd
ChingHui418 <a0985218998@gmail.com>
2026-02-24 15:14:30 +0800

    20260224-1515/jwt

---
+++ src/main/java/tw/hui/spring6/aspect/JwtAspect.java
@@ -0,0 +1,55 @@
+package tw.hui.spring6.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import tw.hui.spring6.exception.JwtAuthException;
+import tw.hui.spring6.util.JwtToken;
+
+@Aspect
+@Component
+public class JwtAspect {
+	private static Logger logger = LoggerFactory.getLogger(JwtAspect.class);
+	
+	@Around("@annotation(tw.hui.spring6.annotation.CheckJwt)")
+	public Object checkJwt(ProceedingJoinPoint joinPoint) throws Throwable {
+		ServletRequestAttributes attributes =
+				(ServletRequestAttributes)(RequestContextHolder.getRequestAttributes());
+		HttpServletRequest request = attributes.getRequest();
+		
+		logger.info(request.getRemoteAddr() + ":" + "Hello");
+		
+		String authHeader = request.getHeader("Authorization");
+		if(authHeader == null) {
+			throw new JwtAuthException("no token");
+		}
+		System.out.println(authHeader);
+		
+		// check bearer token
+		if(!authHeader.startsWith("Bearer ")) {			
+			throw new JwtAuthException("token format error");
+		}
+		
+		try {
+			String token = authHeader.substring(7);
+			String data = JwtToken.parseToken(token);
+			if(data != null) {
+				System.out.println(data);
+			}else {
+				throw new JwtAuthException("token data error");
+			}
+		}catch(Exception e) {
+			throw new JwtAuthException("token parse error:" + e);
+		}
+		
+		return joinPoint.proceed();
+	}
+}
